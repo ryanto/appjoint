81eaa3ca710035af89a08a97f28701bee51a1f78
Ryan Toronto <ryanto@gmail.com>
2022-10-19 20:17:14 -0400

    Start auth off with local persistence

--- packages/demo-app-use-sign-in/components/login.js
+++ packages/demo-app-use-sign-in/components/login.js
@@ -66,6 +66,7 @@ export const Login = function() {
                   id="remember"
                   name="remember"
                   type="checkbox"
+                  defaultChecked
                   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 />
                 <label
