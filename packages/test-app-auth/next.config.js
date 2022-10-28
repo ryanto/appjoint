db0381793f535c23e583857c072ae9ef5e037c27
Ryan Toronto <ryanto@gmail.com>
2022-10-27 21:57:19 -0400

    control which version of react is loaded

--- packages/test-app-auth/next.config.js
+++ packages/test-app-auth/next.config.js
@@ -1,6 +1,12 @@
+let path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev }) => {
+    config.resolve.alias.react = path.join(__dirname, 'node_modules', 'react');
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
